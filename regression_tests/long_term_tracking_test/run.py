c6bb446516af826a674960bc9779b15db89dd96d
JSL1GH <165306874+JSL1GH@users.noreply.github.com>
2026-01-29 15:49:20 -0500

    Fixed path to long_term_tracking

--- regression_tests/long_term_tracking_test/run.py
+++ regression_tests/long_term_tracking_test/run.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-exe = sys.argv[1] + 'long_term_tracking_test'
+#exe = sys.argv[1] + 'long_term_tracking'
+exe = '../../build/bin/' + 'long_term_tracking'
 os.system(exe + ' sim1.init')
 os.system(exe + ' sim2.init')
 os.system('cat sim1.dat sim2.dat > output.now')
